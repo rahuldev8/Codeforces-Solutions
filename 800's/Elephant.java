9500adbdf70ebd4c48a27f203300fa5e6edba9a9
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-19 15:24:07 +0530

    Elephant
    Problem Link to try it Yourself : https://codeforces.com/problemset/problem/617/A

---
+++ 800's/Elephant.java
@@ -0,0 +1,24 @@
+/*
+An elephant decided to visit his friend. It turned out that the elephant's house is located at point 0 and his friend's house is located at point x(x > 0) of the coordinate line. In one step the elephant can move 1, 2, 3, 4 or 5 positions forward. Determine, what is the minimum number of steps he need to make in order to get to his friend's house.
+
+Input
+The first line of the input contains an integer x (1 ≤ x ≤ 1 000 000) — The coordinate of the friend's house.
+
+Output
+Print the minimum number of steps that elephant needs to make to get from point 0 to point x.*/
+
+
+import java.util.*;
+ 
+public class S
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(a%5 == 0)
+        System.out.println(a/5);
+        else 
+        System.out.println((a/5)+1);
+    }
+}
